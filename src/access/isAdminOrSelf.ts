acdf3663fc351f0f9eaeaa1d7a92195d94a9047b
Alain Lilic <alain.lilic1@gmail.com>
2025-03-13 10:18:43 +0100

    push demo

--- src/access/isAdminOrSelf.ts
+++ src/access/isAdminOrSelf.ts
@@ -3,12 +3,12 @@ import { User } from '../payload-types';
 
 export const isAdminOrSelf: Access = ({ req: { user }, id }) => {
   // Return true if user has admin role or is the same user
-  if (user?.roles?.includes('admin')) return true;
+  if (user?.roles?.includes('super-admin')) return true;
   return user?.id === id;
 };
 
 export const isAdminOrSelfFieldLevel: FieldAccess = ({ req: { user }, id }) => {
   // Return true if user has admin role or is the same user
-  if (user?.roles?.includes('admin')) return true;
+  if (user?.roles?.includes('super-admin')) return true;
   return user?.id === id;
 };
